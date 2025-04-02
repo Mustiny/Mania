30d2369dc21ff0279d988a1453c1a3b1ce3b9dc0
Mustiny <mustiny7@gmail.com>
2025-04-02 16:31:28 +0300

    added texture load

--- mania.py
+++ mania.py
@@ -10,6 +10,8 @@ FPS = 60
 WINDOW_W = 800
 WINDOW_H = 600
 
+TEXTURE_PACK = "Default"
+
 FAILED = False
 
 FOLDER_PATH = os.path.dirname(os.path.abspath(__name__))
@@ -39,6 +41,30 @@ if init_config(CONFIG_PATH) == FAILED:
     
 pygame.init()
 
+textures = dict()
+
+def load_textures():
+    textures_path = os.path.join(FOLDER_PATH, "Textures", TEXTURE_PACK)
+    if not os.path.exists(textures_path):
+        print(f"Texture pack folder: {textures_path}', doesn't exists!")
+        TEXTURE_PACK = "Default"
+        if not os.path.exists(os.path.join(FOLDER_PATH, "Textures", TEXTURE_PACK)):
+            print(f"Default texture pack folder doesn't exists!")
+            return FAILED
+    
+    textures_list = ("Note1", "Note2", "Note3", "Note4")
+    for texture in textures_list:
+        texture_path = os.path.join(texture_path, f"{texture}.png")
+        if not os.path.exists(texture_path):
+            texture_path = os.path.join(FOLDER_PATH, "Textures", "Default", texture)
+            if not os.path.exists(texture_path):
+                print(f"Must have texture: '{texture_path}, doesn't exists!'")
+                return FAILED
+        
+        textures[texture] = pygame.image.load(texture_path)
+
+    return True
+
 def game_display():
     pass
 
@@ -57,5 +83,6 @@ def main_inputs():
 def main_loop():
     game_loop()
 
+load_textures()
 main_loop()
 pygame.quit()
